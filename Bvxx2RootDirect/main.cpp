1eefbbe6ef60012ebdd5a6b749764c6cfc84a6a8
haruki-kobayashi <kobayashi.haruki.a1@s.mail.nagoya-u.ac.jp>
2025-04-07 22:50:43 +0900

    BVXXファイルの読み込み処理を改善し、データの取得とツリーへの格納を最適化

--- Bvxx2RootDirect/main.cpp
+++ Bvxx2RootDirect/main.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <sstream>
 #include <VxxReader.h>
 #include <KeywordArgs.h>
 #include <Template.h>
@@ -26,61 +27,46 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Read the BVXX file
-    std::cout << "Reading BVXX file... " << std::endl;
-    vxx::BvxxReader br;
-    std::vector<vxx::base_track_t> btvec = br.ReadAll(bvxxfile, pl_i, 0);
-    if (btvec.empty()) {
-        std::cerr << "Error: Failed to read any data from the input file." << std::endl;
-        return 1;
-    }
-    size_t entries = btvec.size();
-    std::cout << "Read " << entries << " tracks." << std::endl;
-
     // Create the ROOT file and TTree
     TFile* fout = new TFile(outputfile.c_str(), "RECREATE");
     TTree* nt = new TTree("nt", "");
 
-    int RawId, RawId1, RawId2, isg, isg1, isg2;
-    double pl, ax, ay, x, y;
-    double ph1, ax1, ay1, x1, y1, z1, pos1, col1, row1, zone1;
-    double ph2, ax2, ay2, x2, y2, z2, pos2, col2, row2, zone2;
+    int64_t rawid, rawid1, rawid2;
+    int pl, isg1, ph1, pos1, col1, row1, zone1, isg2, ph2, pos2, col2, row2, zone2;
+    double ax, ay, x, y;
+    double ax1, ay1, z1;
+    double ax2, ay2, z2;
 
     // Create branches for the tree
     const std::vector<std::pair<std::string, void*>> intBranches = {
-        {"RawId", &RawId},
-        {"RawId1", &RawId1},
-        {"RawId2", &RawId2},
-        {"isg", &isg},
-        {"isg1", &isg1},
-        {"isg2", &isg2}
-    };
-    const std::vector<std::pair<std::string, void*>> doubleBranches = {
         {"pl", &pl},
-        {"ax", &ax},
-        {"ay", &ay},
-        {"x", &x},
-        {"y", &y},
+        {"rawid", &rawid},
+        {"rawid1", &rawid1},
+        {"rawid2", &rawid2},
+        {"isg1", &isg1},
         {"ph1", &ph1},
-        {"ax1", &ax1},
-        {"ay1", &ay1},
-        {"x1", &x1},
-        {"y1", &y1},
-        {"z1", &z1},
         {"pos1", &pos1},
         {"col1", &col1},
         {"row1", &row1},
         {"zone1", &zone1},
+        {"isg2", &isg2},
         {"ph2", &ph2},
-        {"ax2", &ax2},
-        {"ay2", &ay2},
-        {"x2", &x2},
-        {"y2", &y2},
-        {"z2", &z2},
         {"pos2", &pos2},
         {"col2", &col2},
         {"row2", &row2},
         {"zone2", &zone2}
+    };
+    const std::vector<std::pair<std::string, void*>> doubleBranches = {
+        {"ax", &ax},
+        {"ay", &ay},
+        {"x", &x},
+        {"y", &y},
+        {"ax1", &ax1},
+        {"ay1", &ay1},
+        {"z1", &z1},
+        {"ax2", &ax2},
+        {"ay2", &ay2},
+        {"z2", &z2}
     };
     for (const auto& branch : intBranches) {
         nt->Branch(branch.first.c_str(), branch.second, (branch.first + "/I").c_str());
@@ -89,58 +75,69 @@ int main(int argc, char* argv[]) {
         nt->Branch(branch.first.c_str(), branch.second, (branch.first + "/D").c_str());
     }
 
-    // Fill the tree with data
+    // Process each base track and fill the tree
+    vxx::BvxxReader br;
     int cnt = 0;
-    for (const auto& bt : btvec) {
-        RawId = (int)bt.rawid;
-        pl = (double)bt.pl;
-        isg = (int)bt.isg;
-        ax = (double)bt.ax;
-        ay = (double)bt.ay;
-        x = (double)bt.x;
-        y = (double)bt.y;
+    std::ostringstream oss;
+    if (br.Begin(bvxxfile, pl_i, 0))
+    {
+        vxx::HashEntry h;
+        vxx::base_track_t b;
 
-        ph1 = (double)bt.m[0].ph;
-        ax1 = (double)bt.m[0].ax;
-        ay1 = (double)bt.m[0].ay;
-        x1 = 0.0;
-        y1 = 0.0;
-        z1 = (double)bt.m[0].z;
-        pos1 = (double)bt.m[0].pos;
-        col1 = (double)bt.m[0].col;
-        row1 = (double)bt.m[0].row;
-        zone1 = (double)bt.m[0].zone;
-        RawId1 = (int)bt.m[0].rawid;
-        isg1 = (int)bt.m[0].isg;
+        while (br.NextHashEntry(h))
+        {
+            while (br.NextBaseTrack(b))
+            {
+                rawid = (int64_t)b.rawid;
+                pl = b.pl;
+                ax = b.ax;
+                ay = b.ay;
+                x = b.x;
+                y = b.y;
 
-        ph2 = (double)bt.m[1].ph;
-        ax2 = (double)bt.m[1].ax;
-        ay2 = (double)bt.m[1].ay;
-        x2 = 0.0;
-        y2 = 0.0;
-        z2 = (double)bt.m[1].z;
-        pos2 = (double)bt.m[1].pos;
-        col2 = (double)bt.m[1].col;
-        row2 = (double)bt.m[1].row;
-        zone2 = (double)bt.m[1].zone;
-        RawId2 = (int)bt.m[1].rawid;
-        isg2 = (int)bt.m[1].isg;
+                ph1 = b.m[0].ph;
+                ax1 = b.m[0].ax;
+                ay1 = b.m[0].ay;
+                z1 = b.m[0].z;
+                pos1 = b.m[0].pos;
+                col1 = b.m[0].col;
+                row1 = b.m[0].row;
+                zone1 = b.m[0].zone;
+                rawid1 = b.m[0].rawid;
+                isg1 = b.m[0].isg;
 
-        // Fill the tree with the data
-        nt->Fill();
+                ph2 = b.m[1].ph;
+                ax2 = b.m[1].ax;
+                ay2 = b.m[1].ay;
+                z2 = b.m[1].z;
+                pos2 = b.m[1].pos;
+                col2 = b.m[1].col;
+                row2 = b.m[1].row;
+                zone2 = b.m[1].zone;
+                rawid2 = b.m[1].rawid;
+                isg2 = b.m[1].isg;
 
-        if (cnt % 10000 == 0) {
-            std::cout << "Filled " << cnt << " / " << entries << "\r" << std::flush;
+                // Fill the tree with the data
+                nt->Fill();
+
+                ++cnt;
+                if (cnt % 10000 == 0) {
+                    oss << " Filled " << cnt << " tracks\r";
+                    std::cout << oss.str() << std::flush;
+                    oss.str(""); // Clear the string stream
+                    oss.clear();
+                }
+            }
         }
-        ++cnt;
+        br.End();
     }
 
-    // Write the tree to the file and close it
+    // Save the tree to the file and close the file
     nt->AutoSave();
     fout->Close();
 
-    std::cout << "Filled " << cnt << " / " << entries << std::endl;
-    std::cout << "Finished." << std::endl;
+    std::cout << " Filled " << cnt << " tracks" << std::endl;
+    std::cout << " Finished." << std::endl;
 
     return 0;
 }
